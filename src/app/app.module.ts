a17b11b0f8b5a82fe0b816c48e3dd84eaa48fac9
siyabongahlongwane <siyabonga@webgooru.co.za>
2022-06-10 22:08:44 +0200

    contact page, removed inline styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -38,7 +38,8 @@ import { BecomeAVendorComponent } from './become-avendor/become-avendor.componen
     ReactiveFormsModule,
     HttpClientModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    FormsModule
 
   ],
   providers: [],
--- src/app/home-carousel/home-carousel.component.ts
+++ src/app/home-carousel/home-carousel.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import Splide from './splide.min';
+import { Component, OnInit } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
+import Splide from "./splide.min";
 
 @Component({
-  selector: 'app-home-carousel',
-  templateUrl: './home-carousel.component.html',
-  styles: []
+  selector: "app-home-carousel",
+  templateUrl: "./home-carousel.component.html",
+  styles: [],
 })
 export class HomeCarouselComponent implements OnInit {
   item: any = {
-    url: this.domSanitizer.bypassSecurityTrustStyle("background: linear-gradient(170deg, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 1)), url('../../assets/img/slider-2.jpg'); position: relative !important")
-  }
-  constructor(public domSanitizer: DomSanitizer) { }
+    url: this.domSanitizer.bypassSecurityTrustStyle(
+      "background: linear-gradient(170deg, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 1)), url('../../assets/img/slider-2.jpg'); position: relative !important"
+    ),
+  };
+  constructor(public domSanitizer: DomSanitizer) {}
 
   ngOnInit() {
-    new Splide('.splide', {
-      type: 'loop',
+    new Splide(".splide", {
+      type: "loop",
       perPage: 1,
       speed: 2000,
-      autoplay: 2000
-  }).mount();
+      autoplay: 2000,
+    }).mount();
   }
-
 }
